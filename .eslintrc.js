eaee152b507f0824f355b277b08ee92a6d897692
Bryan Chen <xlchen1291@gmail.com>
2020-06-02 11:49:26 +1200

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const base = require('@polkadot/dev/config/eslint');
+
+module.exports = {
+  ...base,
+  parserOptions: {
+    ...base.parserOptions,
+    project: ['./tsconfig.json']
+  },
+  rules: {
+    ...base.rules,
+    '@typescript-eslint/indent': 'off', // prettier
+    'space-before-function-paren': 'off', // prettier
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-useless-constructor': 'off'
+  }
+};
