b78b968a1d59ca56d816b4cb6a86c18dc049d1dc
Bryan Chen <xlchen1291@gmail.com>
2020-06-03 17:17:21 +1200

    able to kill module storages

--- src/helper.ts
+++ src/helper.ts
@@ -4,8 +4,9 @@ import { options } from '@acala-network/api';
 import { WsProvider, Keyring } from '@polkadot/api';
 import { ApiManager } from '@open-web3/api';
 import { KeyringPair } from '@polkadot/keyring/types';
-import { TransactionResult } from '@open-web3/api/api-manager';
 import { toBaseUnit } from '@open-web3/util';
+import { SubmittableExtrinsic } from '@polkadot/api/types';
+import { xxhashAsU8a } from '@polkadot/util';
 
 dotenv();
 
@@ -24,9 +25,17 @@ export const createApi = async () => {
   };
 };
 
-export const dollar = toBaseUnit;
+export const dollar = (x: BalanceType) => toBaseUnit(x).toFixed();
+
+export type BalanceType = Big | number | string;
+
+function getModulePrefix(module: string): Uint8Array {
+  return xxhashAsU8a(module, 128);
+}
 
 export const setup = async () => {
+  jest.setTimeout(60000);
+
   const keyring = new Keyring({ type: 'sr25519' });
   const apiManager = await ApiManager.create({ ...options(), wsEndpoint: config.ws, account: config.suri, keyring });
   const api = apiManager.api;
@@ -37,15 +46,16 @@ export const setup = async () => {
     api,
     keyring,
     account,
+    tx: api.tx,
     async teardown() {
       this.api.disconnect();
       await new Promise((resolve) => setTimeout(resolve, 2000)); // give some time to cleanup
     },
-    async makeAccounts(count: number, bal: Big | number | string = toBaseUnit(1)) {
+    async makeAccounts(count: number, bal: BalanceType = toBaseUnit(1)) {
       const random = Math.random().toString(16).substr(2, 6);
       const accounts = new Array(count).fill(null).map((_, i) => this.account.derive(`//${random}//${i}`));
       if (bal > 0) {
-        this.transfer(accounts, 0, bal);
+        await this.updateBalance(accounts, api.consts.currencies.nativeCurrencyId.toString(), bal).inBlock;
       }
       console.log(
         'Accounts: ',
@@ -53,25 +63,35 @@ export const setup = async () => {
       );
       return accounts;
     },
-    async transfer(
-      acc: string | KeyringPair | Array<string | KeyringPair>,
-      currency: string | number,
-      val: Big | number | string
-    ) {
+    updateBalance(acc: string | KeyringPair | Array<string | KeyringPair>, currency: string, val: BalanceType) {
       const accounts = ([] as Array<string | KeyringPair>)
         .concat(acc)
         .map((x) => (typeof x === 'string' ? x : x.address));
 
       return this.apiManager.signAndSend(
-        this.api.tx.utility.batch(accounts.map((a) => this.api.tx.currencies.transfer(a, currency, val.toString())))
-      ).inBlock;
+        accounts.map((a) =>
+          this.api.tx.sudo.sudo(this.api.tx.currencies.updateBalance(a, currency, new Big(val).toFixed()))
+        )
+      );
+    },
+    async feedPrice(currency: string, price: BalanceType) {
+      const index = 0; // TODO: lookup via available session key
+      const values = [[currency, new Big(price).toFixed()]];
+      const nonce = await this.api.query.oracle.nonces(this.account.address);
+      const payload = this.api.registry.createType('(u32, Vec<(CurrencyId, Price)>)' as any, [nonce, values]);
+      const sig = this.account.sign(payload.toU8a());
+      await this.api.tx.oracle.feedValues(values, index, sig).send();
+    },
+    send(call: SubmittableExtrinsic<'promise'>, account?: KeyringPair) {
+      return this.apiManager.signAndSend(call, { account });
+    },
+    sudo(call: SubmittableExtrinsic<'promise'>) {
+      return this.send(this.api.tx.sudo.sudo(call));
+    },
+    killModuleStorage(module: string) {
+      return this.sudo(this.tx.system.killPrefix(getModulePrefix(module), 0));
     }
   };
-};
-
-export const assertSuccess = (res: TransactionResult) => {
-  const success = res.events.some((evt) => evt.event.section === 'System' && evt.event.method === 'ExtrinsicSuccess');
-  expect(success).toBeTruthy();
 };
 
 type Await<T> = T extends {
--- src/init.test.ts
+++ src/init.test.ts
@@ -11,7 +11,23 @@ afterAll(async () => {
 });
 
 test('test', async () => {
-  const accounts = await ctx.makeAccounts(1);
-  await ctx.transfer(accounts, 'XBTC', dollar(1));
-  console.log((await ctx.api.query.tokens.accounts(accounts[0].address, 'XBTC')).toHuman());
+  const accounts = await ctx.makeAccounts(2);
+  const [alice, bob] = accounts;
+  await ctx.updateBalance(alice, 'XBTC', dollar(10)).send;
+  await ctx.updateBalance(bob, 'XBTC', dollar(101)).send;
+  await ctx.updateBalance(bob, 'AUSD', dollar(1_000_000)).send;
+  await ctx.feedPrice('XBTC', dollar(10000));
+  await ctx.send(ctx.tx.dex.addLiquidity('XBTC', dollar(100), dollar(1_000_000))).send;
+  await ctx.sudo(
+    ctx.tx.cdpEngine.setCollateralParams(
+      'XBTC',
+      0, // stability_fee
+      dollar(2), // liquidation_ratio
+      dollar('0.2'), // liquidation_penalty
+      dollar(2), // required_collateral_ratio
+      dollar(1000000) // maximum_total_debit_value
+    )
+  ).send;
+  await ctx.send(ctx.tx.honzon.adjustLoan('XBTC', dollar(10), dollar(500_000)), alice).send;
+  await ctx.send(ctx.tx.honzon.adjustLoan('XBTC', dollar(1), dollar(50_000)), bob).inBlock;
 });
