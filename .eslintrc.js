374b992fcb5119c7f93baff7719e737099b81172
Ermal Kaleci <ermalkaleci@gmail.com>
2020-06-08 11:33:44 +0200

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,13 @@ module.exports = {
     '@typescript-eslint/indent': 'off', // prettier
     'space-before-function-paren': 'off', // prettier
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     'no-useless-constructor': 'off',
     'header/header': 'off',
     'sort-keys': 'off',
-    'comma-dangle': 'off'
+    'comma-dangle': 'off',
+    'padding-line-between-statements': 'off'
   }
 };
