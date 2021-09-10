a547463c0fa8a1d7d9390315053a65a3cba8edbc
Ermal Kaleci <ermalkaleci@gmail.com>
2021-09-10 14:45:01 +0200

    update simulate liquidate to use DOT

--- src/simulate-liquidate-cdp.ts
+++ src/simulate-liquidate-cdp.ts
@@ -1,18 +1,20 @@
-import { Context, setup, rate, ratio, price, aca, ausd, renbtc } from './helper';
+import { Context, setup, rate, ratio, price, aca, ausd, dot } from './helper';
 
 const ACA = { token: 'ACA' };
 const AUSD = { token: 'AUSD' };
-const RENBTC = { token: 'RENBTC' };
+const DOT = { token: 'DOT' };
+
+const debitExchangeRate = 0.1;
 
 export const simulateLiquidateCDP = async () => {
   const ctx: Context = await setup();
   const [alice, bob] = await ctx.makeAccounts(2);
 
   await ctx.updateBalance(alice, AUSD, ausd(10_000_000)).send;
-  await ctx.updateBalance(alice, RENBTC, renbtc(10_000_000)).send;
+  await ctx.updateBalance(alice, DOT, dot(10_000_000)).send;
 
   await ctx.updateBalance(bob, AUSD, ausd(1_000_000)).send;
-  await ctx.updateBalance(bob, RENBTC, renbtc(101)).send;
+  await ctx.updateBalance(bob, DOT, dot(101)).send;
 
   const bidder = process.env.BIDDER_ADDRESS;
   if (bidder) {
@@ -21,11 +23,11 @@ export const simulateLiquidateCDP = async () => {
     await ctx.updateBalance(bidder, ACA, aca(1_000)).send;
   }
 
-  await ctx.feedPrice(RENBTC, price(10_000)).send;
-  await ctx.send(ctx.tx.dex.addLiquidity(AUSD, RENBTC, ausd(1_000_000), renbtc(100), 0, false), alice).send;
+  await ctx.feedPrice(DOT, price(30)).send;
+  await ctx.send(ctx.tx.dex.addLiquidity(AUSD, DOT, ausd(30_000), dot(1_000), 0, false), alice).send;
   await ctx.sudo(
     ctx.tx.cdpEngine.setCollateralParams(
-      RENBTC,
+      DOT,
       { NewValue: rate(0) }, // stability_fee
       { NewValue: ratio(2) }, // liquidation_ratio
       { NewValue: rate('0.2') }, // liquidation_penalty
@@ -33,9 +35,9 @@ export const simulateLiquidateCDP = async () => {
       { NewValue: ausd(1_000_000) } // maximum_total_debit_value
     )
   ).send;
-  await ctx.send(ctx.tx.honzon.adjustLoan(RENBTC, renbtc(10), ausd(500_000)), alice).inBlock;
-  await ctx.send(ctx.tx.honzon.adjustLoan(RENBTC, renbtc(1), ausd(50_000)), bob).inBlock;
+  await ctx.send(ctx.tx.honzon.adjustLoan(DOT, dot(10), ausd(150 / debitExchangeRate)), alice).inBlock;
+  await ctx.send(ctx.tx.honzon.adjustLoan(DOT, dot(1), ausd(15 / debitExchangeRate)), bob).inBlock;
 
-  await ctx.tx.cdpEngine.liquidate(RENBTC, alice.address).send();
+  await ctx.tx.cdpEngine.liquidate(DOT, alice.address).send();
   await ctx.teardown();
 };
