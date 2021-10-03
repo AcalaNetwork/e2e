ec81237cf075d80d3cb21ad9b6e2a225818f49a4
Ermal Kaleci <ermalkaleci@gmail.com>
2021-10-03 13:45:46 +0200

    update simulate-loan

--- src/simulate-loan.ts
+++ src/simulate-loan.ts
@@ -1,28 +1,28 @@
-import { Context, setup, rate, ratio, price, aca, ausd, renbtc } from './helper';
+import { Context, setup, rate, ratio, price, aca, ausd, dot } from './helper';
 
 const ACA = { Token: 'ACA' };
 const AUSD = { Token: 'AUSD' };
-const RENBTC = { Token: 'RENBTC' };
+const DOT = { Token: 'DOT' };
 
 export const simulateLoan = async () => {
   const ctx: Context = await setup();
   const [alice] = await ctx.makeAccounts(1);
 
   await ctx.updateBalance(alice, AUSD, ausd(10_000_000)).send;
-  await ctx.updateBalance(alice, RENBTC, renbtc(10_000_000)).send;
+  await ctx.updateBalance(alice, DOT, dot(10_000_000)).send;
 
   const address = process.env.ADDRESS;
   if (address == null || address.length === 0) throw Error('process.env.ADDRESS not defined!');
 
   await ctx.updateBalance(address, ACA, aca(1_000)).send;
   await ctx.updateBalance(address, AUSD, ausd(10_000_000)).send;
-  await ctx.updateBalance(address, RENBTC, renbtc(10_000)).send;
+  await ctx.updateBalance(address, DOT, dot(10_000)).send;
 
-  await ctx.feedPrice(RENBTC, price(10_000)).send;
-  await ctx.send(ctx.tx.dex.addLiquidity(AUSD, RENBTC, ausd(1_000_000), renbtc(100), 0, false), alice).send;
+  await ctx.feedPrice(DOT, price(30)).send;
+  await ctx.send(ctx.tx.dex.addLiquidity(AUSD, DOT, ausd(1_000_000), dot(33_000), 0, false), alice).send;
   await ctx.sudo(
     ctx.tx.cdpEngine.setCollateralParams(
-      RENBTC,
+      DOT,
       { NewValue: rate(0) }, // stability_fee
       { NewValue: ratio(1.2) }, // liquidation_ratio
       { NewValue: rate('0.2') }, // liquidation_penalty
@@ -31,8 +31,8 @@ export const simulateLoan = async () => {
     )
   ).send;
 
-  const tx = ctx.tx.honzon.adjustLoan(RENBTC, renbtc(1), ausd(50_000));
+  const tx = ctx.tx.honzon.adjustLoan(DOT, dot(1), ausd(200));
   await ctx.send(ctx.tx.sudo.sudoAs(address, tx)).inBlock;
-  await ctx.feedPrice(RENBTC, price(7_000)).inBlock;
+  await ctx.feedPrice(DOT, price(27)).inBlock;
   await ctx.teardown();
 };
